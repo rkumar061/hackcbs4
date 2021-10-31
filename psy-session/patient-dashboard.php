6f84ff858eec2fa98a8cd654039f16bed758e23e
Ushnish Dey Bhowmik <neilbhowmik05@gmail.com>
2021-10-31 11:59:55 +0530

    Patient Dashboard Frontend Complete
    Add Backend for further commit.

--- psy-session/patient-dashboard.php
+++ psy-session/patient-dashboard.php
@@ -15,15 +15,199 @@ if (isset($_SESSION['uid'])){
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+<style>
+    .profile-wrapper{
+        margin:0px;
+        display: flex;
+        justify-content: space-between;
+        align-items:center;
+        padding:40px;
+        font-family: 'Ubuntu', sans-serif;
+        color: rgb(224,255,255);
+    }
+
+    .patient-name{
+        font-size:50px;
+        font-weight: medium;
+    }
+
+    .patient-id{
+        padding-top:4px;
+    }
+
+    .profile-btn-wrapper{
+        width: 25%;
+        display:flex;
+        justify-content: space-between;
+        align-items:center;
+        font-family: inherit;
+    }
+
+    .meetings-btn{
+      background:rgb(224,255,255);
+      color: rgba(0,0,0,1);
+      padding: 15px;
+      border-radius:15px;
+      border: none;
+      transition: 0.3s;
+      font-family: inherit;
+      font-weight:bold;
+      font-size: 18px;
+    }
+
+    .meetings-btn:hover{
+        background:none;
+      color: rgb(224,255,255);
+      padding: 15px;
+      font-weight: bold;
+      border-radius: 30px;
+      border: 3px solid rgb(224,255,255); 
+      font-family: inherit;
+      font-size: 18px;
+
+    }
+
+    .anchor-btn{
+      margin: 0px;
+      padding: 0px;
+      font-family: inherit;
+      text-decoration: none;
+      color: inherit;
+    }
+
+    .content-wrapper{
+      padding-left:10px;
+      padding-right:10px;
+      margin:0px;
+    }
+
+    .table-general{
+        width:100%;
+        align: center;
+        background: none;
+        border: 0px;
+        box-shadow: rgba(0, 0, 0, 0.4) 0px 2px 4px, rgba(0, 0, 0, 0.3) 0px 7px 13px -3px, rgba(0, 0, 0, 0.2) 0px -3px 0px inset;
+    }
+
+    .table-general thead{
+        background-color: rgba(0,0,0,0.5);
+        color: rgb(224,255,255);
+        text-align: center;
+        font-family: 'Ubuntu', sans-serif;
+        font-size:18px;
+    }
+
+    .table-general th{
+        padding-left: 15px;
+        padding-right: 15px;
+        padding-top: 10px;
+        margin:0px; 
+    }
+
+    .table-general tbody{
+        background-color: rgba(0,0,0, 0.7);
+        color: rgb(224,255,255);
+        text-align: center;
+        font-family: 'Ubuntu', sans-serif;
+        font-size: 15px;
+    }
+
+    .table-general td{
+        padding-left: 15px;
+        padding-right: 15px;
+        padding-top: 10px;
+        padding-bottom: 10px;
+    }
+
+    .table-p-btn{
+      background:rgb(224,255,255);
+      color: rgba(0,0,0,1);
+      padding: 6px;
+      border-radius:0px;
+      border: none;
+      transition: 0.3s;
+      font-family: inherit;
+      font-weight:bold;
+    }
+
+    .table-p-btn:hover{
+      background:none;
+      color: rgb(224,255,255);
+      padding: 6px;
+      font-weight: bold;
+      border-radius: 15px;
+      border: 3px solid rgb(224,255,255); 
+      font-family: inherit;
+    }
+
+    .table-header{
+        font-family:'Ubuntu', sans-serif;
+        font-size: 25px;
+        font-weight:medium;
+        color: rgb(224,255,255);
+    }
+
+</style>
+
 <body>
     <?php include 'header.php' ?>
 
     <section class="profile-wrapper">
         <div>
             <span class="patient-name">John Doe</span><br><br>
-            <span class="patient-id">12</span>
+            <span class="patient-id">Your Patient ID: 12</span>
         </div>
-        <div>
-            <
+        <div class="profile-btn-wrapper">
+            <button class="meetings-btn new-application"><a class="anchor-btn" href="#">New Application</a></button>
+        </div>
+</section>
+<section class="content-wrapper">
+<section>
+    <br><br><span class="table-header">Active Sessions</span><br><br>
+    <table cellspacing="0px" class="table-general current-session-table">
+        <thead>
+            <tr>
+                <th><h1>Psychologist Name</h1></th>
+                <th><h1>Problem</h1></th>
+                <th><h1>Remarks</h1></th>
+                <th><h1>Schedule</h1></th>
+                <th><h1>Meeting Link</h1></th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td class="table-psy-name">Eminem</td>
+                <td class="table-p-problem">Love Cancer</td>
+                <td class="table-psy-remarks">Mar jaa bsdk</td>
+                <td class="table-session-schedule">69/69/6969</td>
+                <td class="table-meeting-link"><button class="table-p-btn"><a href="#" class="anchor-btn">Join Now</a></button></td>
+            </tr>
+        </tbody>
+    </table>
+</section>
+<br><br><span class="table-header">Old Sessions</span><br><br>
+<section>
+    <table cellspacing="0px" class="table-general old-session-table">
+        <thead>
+            <tr>
+                <th><h1>Psychologist Name</h1></th>
+                <th><h1>Problem</h1></th>
+                <th><h1>Remarks</h1></th>
+                <th><h1>Schedule</h1></th>
+                <th><h1>Reports</h1></th>
+            </tr>
+        </thead> 
+        <tbody>
+            <tr>
+                <td class="table-psy-name">Eminem</td>
+                <td class="table-p-problem">Love Cancer</td>
+                <td class="table-psy-remarks">Mar jaa bsdk</td>
+                <td class="table-session-schedule">69/69/6969</td>
+                <td class="table-meeting-link"><button class="table-p-btn"><a href="report.php" class="anchor-btn">Report</a></button></td>
+            </tr>
+        </tbody> 
+    </table>
+</section>
+</section>     
 </body>
 </html>
